005abba4cbbb86048e1df9a8e50dd846e43ecbd7
Rob Culhane <roblculhane@gmail.com>
2021-06-05 00:34:56 -0400

    Initial setup

---
+++ Assets/RatherGood/MMOKit/Scripts/UISceneGameplay_RatherGoodShowCursorMod.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.Serialization;
+
+namespace MultiplayerARPG
+{
+    //Rather Good Show Cursor mod will show cursor on keypress
+
+    public partial class UISceneGameplay : BaseUISceneGameplay
+    {
+
+        [Header("Rather Good Toggle Cursor Mod.")]
+
+        private bool buttonToggleCursorShowPressed = false;
+
+        [Tooltip("Add this button name and key to GameInstance InputSettingsManager.")]
+        [SerializeField] string toggleCursorButtonName = "ToggleCursorModButton";
+
+        /// <summary>
+        /// Called from ShooterPlayerCharacterController Update()
+        /// Overrides BaseUISceneGameplay IsBlockController() to add user controlled show cursor button
+        /// </summary>
+        /// <returns></returns>
+        public override bool IsBlockController()
+        {
+
+            if (base.IsBlockController())
+            {
+                //Toggle cursor show off once a UI takes over.
+                //This will return to normal operation as soon as the UI is closed.
+                buttonToggleCursorShowPressed = false;
+                return true;
+            }
+
+            if (InputManager.GetButtonDown(toggleCursorButtonName))
+            {
+                buttonToggleCursorShowPressed = !buttonToggleCursorShowPressed;
+            }
+
+            return buttonToggleCursorShowPressed;
+        }
+
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # ShowCursor
-Provides hotkey to display cursor without bringing up a dialog for Suriyun MMORPG Kit
+
+**Author:** RLC
+
+**Description:** Provides hotkey to display cursor without bringing up a dialog
+for Suriyun MMORPG Kit.
+
+**Other Dependencies:** None
+
+**Core MMORPG Kit modifications:** NONE
+
+**Instructions for use:**
+
+1.  Drop script anywhere in project or import UnityPackage from GitHub.
+
+2.  In your “Init” scene select GameInstance. Click the UISceneGameplayPrefab
+    will take you directly to the prefab.
+
+3.  In the UISceneGameplay component find the “toggleCursorButtonName” and set
+    the text as desired.
+
+    ![Graphical user interface, text Description automatically
+    generated](media/9d82f6583bd7377d6343e04e31157bc9.png)
+
+4.  In your “Init” scene select GameInstance. Find the InputSettingsManager
+    component. Add a new key. keyName must be exactly the same as previous.
+
+5.  Set the keycode to be whatever key you want to use to show the cursor.
+
+![Graphical user interface, text, application, email Description automatically
+generated](media/001153dd92eb5c0505d413de1a300129.png)
+
+**Done.**
+
+Hit play and press the button you selected to show the cursor.
+
+<https://youtu.be/Wbf0DS2OH38>
